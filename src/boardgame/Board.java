d84330e4afa0d93db85d9cbc434ced9220f5279e
Richard Bringmann <richardbringmann160301@gmail.com>
2020-07-06 22:54:02 -0300

    added function to move pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -43,6 +43,17 @@ public class Board {
 		piece.position = pos;
 	}
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position))
+			throw new BoardException("Invalid position");
+		if(piece(position) == null)
+			return null;
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
@@ -58,6 +69,8 @@ public class Board {
 			throw new BoardException("Invalid position");
 		return piece(position) != null;
 	}
+	
+	
 	
 	
 	
